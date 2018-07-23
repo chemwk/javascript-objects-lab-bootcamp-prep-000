4efbe47ddc7bcfc9a14733c3c1569ffb5cb7d3ff
min kim <minwookkim2013@u.northwestern.edu>
2018-07-23 23:31:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object.key = [key]:value
+  object.[key] = {[key]:value}
   return object
 }
 
