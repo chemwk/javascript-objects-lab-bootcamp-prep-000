31d6c079845570ac23b79ef896e39801dfcdfdfa
min kim <minwookkim2013@u.northwestern.edu>
2018-07-23 23:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var recipes = {a:"b"}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign({},object)
+  return Object.assign({},object,[key]:value)
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object.[key] = [key]:value
+  return object
+}
+
+function deleteFromObjectByKey()
